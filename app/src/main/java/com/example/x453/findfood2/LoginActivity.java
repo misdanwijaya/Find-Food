bc5a736c2e8bfd56756250be1ab0853c03d590ca
misdanwijaya <misdan.wijaya07@gmail.com>
2016-11-15 10:57:42 +0700

    First Commit

---
+++ app/src/main/java/com/example/x453/findfood2/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.example.x453.findfood2;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    static final int ACT2_REQUEST = 99;  // request code
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void button(View v) {
+        //ambil komponen text dan label
+        EditText mail = (EditText) findViewById(R.id.mail);
+        EditText pass = (EditText) findViewById(R.id.pass);
+
+        //cek login
+        //when login is correct, will be directly to MapsActivity
+        if(mail.getText().toString().equals("misdan@gmail.com") && pass.getText().toString().equals("1234")){
+
+            //show toast
+            Toast toast = Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_LONG);
+            toast.show();
+
+            //clear the textfield
+            mail.setText("");
+            pass.setText("");
+
+            Intent intent2 = new Intent(this, MapsActivity.class);
+            startActivityForResult(intent2,ACT2_REQUEST);
+
+            //when login is failed
+        }else{
+
+            //show toast
+            Toast toast2 = Toast.makeText(getApplicationContext(), "Login Gagal", Toast.LENGTH_LONG);
+            toast2.show();
+
+            //clear the text field
+            mail.setText("");
+            pass.setText("");
+        }
+    }
+}
